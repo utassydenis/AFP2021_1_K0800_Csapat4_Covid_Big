50b086f237dec49381f096af3a7504570b2b4bb8
Dénes Utassy <utassy.denis@gmail.com>
2021-11-19 15:39:37 +0100

    Uploaded code responsible for window behaviour

--- code/main.py
+++ code/main.py
@@ -3,6 +3,11 @@ from kivy.app import  App
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 
+Builder.load_file("kv-files/covid.kv")
+Config.set("graphics", "width", "1050")
+Config.set("graphics", "height", "850")
+Config.set("graphics", "resizable", "0")
+
 class Covid(FloatLayout):
     def __init__(self, **kwargs):
         super(Covid, self).__init__(**kwargs)
