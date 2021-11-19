2b63a4c4b932166038c2ebc6a74a7aeb4a068527
Dénes Utassy <utassy.denis@gmail.com>
2021-11-19 15:40:56 +0100

    Uploaded code responsible for calling the functions to updating database

--- code/main.py
+++ code/main.py
@@ -12,6 +12,20 @@ class Covid(FloatLayout):
     def __init__(self, **kwargs):
         super(Covid, self).__init__(**kwargs)
 
+    def update_db(self):
+        print("Update started!")
+        self.dir_exist()
+        if not os.path.exists("database/data.csv"):
+            self.download_cvs()
+        self.make_Trimmed_file()
+        self.trim_data()
+        self.check_if_db_exists()
+        self.check_if_table_exists()
+        if not self.check_if_table_exists():
+            self.create_table()
+        else:
+            self.reset_db()
+        print("Done updating")
 
 class CovidApp(App):
     def build(self):
