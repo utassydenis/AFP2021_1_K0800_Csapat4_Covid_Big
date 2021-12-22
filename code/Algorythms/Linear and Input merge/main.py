f1e1220a0587cd3d1560062246ef85a1e96f73ca
Rados Kevin <rkevin99@gmail.com>
2021-12-22 14:01:37 +0100

    Final algorythm's data prediction

--- code/Algorythms/Linear and Input merge/main.py
+++ code/Algorythms/Linear and Input merge/main.py
@@ -20,3 +20,21 @@ y0 = model.predict(x)
 # plt.plot(y0, '--b')
 # plt.show()
 
+#### PREDICTION ####
+days = 14
+print('-'*30);print('PREDICTION');print('-'*30)
+print(f'Prediction - Cases after {days} days: ', end='')
+print(round(int(model.predict(polyFeat.fit_transform([[284 + days]])))), 'people')
+
+x1 = np.array(list(range(1, 284+days))).reshape(-1, 1)
+y1 = model.predict(polyFeat.fit_transform(x1))
+
+plt.title('Modell')
+plt.xlabel('Napok')
+plt.ylabel('Megbetegedések')
+plt.legend(['Eredeti'])
+plt.plot(y1, '--r')
+plt.plot(y0, '--b')
+plt.show()
+
+
