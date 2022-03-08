0726ba06802b0bd4b67f4bdbb1440d8f2f14eeb1
Milán <suhaj73@gmail.com>
2022-03-08 18:06:39 +0100

    Added prediction_algorythm method to main.py.

--- code/main.py
+++ code/main.py
@@ -7,6 +7,12 @@ import requests
 import os.path
 import mysql.connector
 import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+import csv
 
 Builder.load_file("kv-files/covid.kv")
 Config.set("graphics", "width", "1050")
@@ -17,28 +23,49 @@ class Covid(FloatLayout):
     def __init__(self, **kwargs):
         super(Covid, self).__init__(**kwargs)
 
-    #To-do eleje Milán és Bálint
-    def calculate_infected_total_cases(self):
-        mydb = mysql.connector.connect(host="localhost", user="root", database="covid_database")
-        mycursor = mydb.cursor()
+    #To-do eleje Milán
+    def prediction_algoryth(day: int, column: str, data: str):
+        file = open(data + ".csv")
+        reader = csv.reader(file)
+        lines = int(len(list(reader))) - 1
+        print('Number of rows: ', lines)
 
-        mycursor.close()
-        mydb.close()
+        data = pd.read_csv(data + '.csv', sep=';')
+        data = data[['day', column]]
+        print('-' * 30);
+        print('HEAD');
+        print('-' * 30)
+        print(data.head())
 
-    def calculate_death_total_cases(self):
-        mydb = mysql.connector.connect(host="localhost", user="root", database="covid_database")
-        mycursor = mydb.cursor()
+        print('-' * 30);
+        print('PREPARE DATA');
+        print('-' * 30)
+        x = np.array(data['day']).reshape(-1, 1)
+        y = np.array(data[column]).reshape(-1, 1)
+        plt.plot(y, '-m')
 
-        mycursor.close()
-        mydb.close()
+        polyFeat = PolynomialFeatures(degree=4)
+        x = polyFeat.fit_transform(x)
 
-    def distinct_countries(self):
-        mydb = mysql.connector.connect(host="localhost", user="root", database="covid_database")
-        mycursor = mydb.cursor()
+        print('-' * 30);
+        print('TRAINING DATA');
+        print('-' * 30)
+        model = linear_model.LinearRegression()
+        model.fit(x, y)
+        accuracy = model.score(x, y)
+        print(f'Accuracy:{round(accuracy * 100, 3)} %')
+        y0 = model.predict(x)
 
-        mycursor.close()
-        mydb.close()
-    #To-do vége Milán és Bálint
+        days = day
+        print('-' * 30);
+        print('PREDICTION');
+        print('-' * 30)
+        print(f'Prediction - Cases after {days} days: ', end='')
+        print(round(int(model.predict(polyFeat.fit_transform([[lines + days]])))), 'people')
+
+        x1 = np.array(list(range(1, lines + days))).reshape(-1, 1)
+        y1 = model.predict(polyFeat.fit_transform(x1))
+    #To-do vége Milán
         
     # ----- update_db által meghívott fgvnyek -----
 
